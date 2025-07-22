e4c1b32e520e8b61a7ce844c1932a1e6c0355fa4
Clinton Nguyen <clintonnguyen@gmail.com>
2025-07-22 10:00:22 -0700

    last resort

--- play.pokemonshowdown.com/js/client-battle.js
+++ play.pokemonshowdown.com/js/client-battle.js
@@ -1,4 +1,5 @@
 const TURN_TIME = 25;
+const clientInfoLog = [];
 (function ($) {
 
 	const hoverLogs = {
@@ -1234,6 +1235,7 @@ const TURN_TIME = 25;
 				hoverLogs: hoverLogs,
 			}
 			console.log(clientInfo)
+			clientInfoLog.push(clientInfo);
 			// this.send(`/choose fromclient ${JSON.stringify(clientInfo)}`);
 			this.send(buf.substr(0, buf.length - 1) + '|' + this.request.rqid);
 		},
@@ -1256,6 +1258,7 @@ const TURN_TIME = 25;
 				hoverLogs: hoverLogs,
 			}
 			console.log(clientInfo)
+			clientInfoLog.push(cliengInfo);
 			// this.send(`/choose fromclient ${JSON.stringify(clientInfo)}`);
 			if (!this.actionSent) this.send('/choose move ' + randomNum + '|'+ this.request.rqid);
 		},

c6049682c733997a8766ef04cbbd8758a852c09e
Clinton Nguyen <clintonnguyen@gmail.com>
2025-07-24 18:29:28 -0700

    removing import

--- play.pokemonshowdown.com/config/testclient-key.js
+++ play.pokemonshowdown.com/config/testclient-key.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+// require('dotenv').config();
 
 // exports.CLIENT_KEY = process.env.POKEMON_SHOWDOWN_TESTCLIENT_KEY;
 window.CLIENT_KEY = 'Obbygun1,136793483,45435cfd4d52a6c31745301070';

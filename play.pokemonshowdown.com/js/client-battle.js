354a73f2895f125ada34f7e1c09f613b3b17af5f
Clinton Nguyen <clintonnguyen@gmail.com>
2025-07-30 10:41:38 -0700

    adding rqid

--- play.pokemonshowdown.com/js/client-battle.js
+++ play.pokemonshowdown.com/js/client-battle.js
@@ -1236,8 +1236,9 @@ const clientInfoLog = [];
 			}
 			console.log(clientInfo)
 			console.log(this)
+
+			this.send(`/choose fromclient ${JSON.stringify(clientInfo)}|${this.request.rqid}`);
 			this.send(buf.substr(0, buf.length - 1) + '|' + this.request.rqid);
-			// this.send(`/choose fromclient ${JSON.stringify(clientInfo)}`);
 		},
 
 		// sendRandom: function () {
